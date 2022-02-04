11e5facc4cc556fc9ae71d43c3e36f85b49fe529
Ripenen <0>
2022-02-04 15:57:16 +0800

    Fix

--- Assets/Base/Scripts/UI/LevelMenu.cs
+++ Assets/Base/Scripts/UI/LevelMenu.cs
@@ -7,17 +7,17 @@ namespace Base.Scripts.UI
 {
     public class LevelMenu : MonoBehaviour
     {
-        [SerializeField] private List<SceneAsset> _levels = new List<SceneAsset>();
+        [SerializeField] private List<string> _levelNames = new List<string>();
         [SerializeField] private CustomButton _buttonPrefab;
         [SerializeField] private GameObject _contentObject;
 
         private void Awake()
         {
-            foreach (var level in _levels)
+            foreach (var level in _levelNames)
             {
                 var customButton = Instantiate(_buttonPrefab, _contentObject.transform);
-                customButton.Change(level.name);
-                customButton.Clicked.AddListener(() => SceneManager.LoadScene(level.name));
+                customButton.Change(level);
+                customButton.Clicked.AddListener(() => SceneManager.LoadScene(level));
             }
         }
     }
